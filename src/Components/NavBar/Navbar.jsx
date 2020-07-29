7bc2055e3876cf84dbe53aa25c4ff20e92bea1f0
Alex Turcu <turcu.alex5@gmail.com>
2020-07-29 11:06:43 +0300

    adding starting project

---
+++ src/Components/NavBar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./Navbar.scss"
+
+const Navbar = () => (
+
+    <div className="navbar">
+      <h3 className="logo">Crazy <span>LOOK</span></h3>
+    </div>
+)
+
+export default Navbar;
---
+++ src/Pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navbar from '../Components/NavBar/Navbar'
+import ProductsGrid from '../Components/ProductsGrid'
+
+const HomePage = ()=> (
+    <div>
+        <Navbar />
+        <ProductsGrid />
+      
+    </div>
+)
+
+export default HomePage;
